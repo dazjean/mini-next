c3c0299da6d144311583231e514da89194567356
zhangjiajia04 <zhangjiajia04@58ganji.com>
2018-11-19 11:12:37 +0800

    更新node-sass模块版本

--- server/config_ssr.js
+++ server/config_ssr.js
@@ -1,3 +1,3 @@
 module.exports = {
-    ssrPageFilter:['favicon.ico','vendor','chejian','cityshow'] //过滤掉不需要服务端渲染的页面 'fxcformcommon_server','fxcformcommon_server_router'
+    ssrPageFilter:['favicon.ico','vendor'] //过滤掉不需要服务端渲染的页面 'fxcformcommon_server','fxcformcommon_server_router'
 }
