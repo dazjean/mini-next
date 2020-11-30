757a76004797d6e5d15e804a3701d74a0809964a
dazjean <zunyi_zjj@163.com>
2020-11-30 13:06:03 +0800

    fix:home.tsx

--- src/pages/_home/_home.tsx
+++ src/pages/_home/_home.tsx
@@ -7,13 +7,13 @@ export default function (props:typeProps){
     return (
         <div className ='ts-demo' >
              <ul>
-                <li><a href="/with-react/with-react.html">with-react</a></li>
-                <li><a href="/with-react-browserRouter/with-react-browserRouter.html">with-react-browserRouter</a></li>
-                <li><a href="/with-react-hashRouter/with-react-hashRouter.html">with-react-hashRouter</a></li>
-                <li><a href="/with-isomorphic-fetch/with-isomorphic-fetch.html">with-isomorphic-fetch</a></li>
-                <li><a href="/with-async-await-fetch/with-async-await-fetch.html">with-async-await-fetch</a></li>
-                <li><a href="/with-react-hooks/with-react-hooks.html">with-react-hooks</a></li>
-                <li><a href="/with-typescript/with-typescript.html">with-typescript</a></li>
+                <li><a href="/with-react">with-react</a></li>
+                <li><a href="/with-react-browserRouter">with-react-browserRouter</a></li>
+                <li><a href="/with-react-hashRouter">with-react-hashRouter</a></li>
+                <li><a href="/with-isomorphic-fetch">with-isomorphic-fetch</a></li>
+                <li><a href="/with-async-await-fetch">with-async-await-fetch</a></li>
+                <li><a href="/with-react-hooks">with-react-hooks</a></li>
+                <li><a href="/with-typescript">with-typescript</a></li>
             </ul>
         </div>
     )
