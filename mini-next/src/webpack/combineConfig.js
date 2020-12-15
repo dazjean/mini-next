2c96e52310de48362bd26536d66ba705ef1d8146
龙丹 <1060921782@qq.com>
2020-12-15 16:44:46 +0800

    [feat]:webpack自定义配置扩展

--- mini-next/src/webpack/combineConfig.js
+++ mini-next/src/webpack/combineConfig.js
@@ -20,7 +20,7 @@ function isSameLoader(loader1, loader2) {
     const loader2Name = typeof loader2 === 'string' ? loader2 : loader2.loader;
     return loader1Name === loader2Name;
 }
-function mergeLoader(before, item, cssLoader = false, server = false) {
+function mergeLoader(before, item) {
     if (item && item.length) {
         //如果有则覆盖，没有则添加 双指针：一个beforeIndex指向原有默认配置，一个itemIndex指向用户配置
         //1.loader相同 beforeIndex++ && itemIndex++,覆盖
@@ -44,17 +44,12 @@ function mergeLoader(before, item, cssLoader = false, server = false) {
         }
     }
 }
-module.exports = function(config, server) {
-    if (!fs.existsSync(userWebpackConfigPath)) {
-        return config;
-    }
-    delete require.cache[require.resolve(userWebpackConfigPath)];
-    const userConfig = require(userWebpackConfigPath).webpack;
-    if (!userConfig) return config;
-    let defaultLoader = config.module.rules;
+//loader单独处理
+function loaderConfig(userConfigLoader, config) {
     //添加loader
-    userConfig.loader &&
-        Object.entries(userConfig.loader).map(([key, item]) => {
+    let defaultLoader = config.module.rules;
+    userConfigLoader &&
+        Object.entries(userConfigLoader).map(([key, item]) => {
             switch (key) {
                 case 'js':
                     mergeLoader(defaultLoader[0], item);
@@ -69,13 +64,13 @@ module.exports = function(config, server) {
                     mergeLoader(defaultLoader[3], item);
                     break;
                 case 'css':
-                    mergeLoader(defaultLoader[4], item, true, server);
+                    mergeLoader(defaultLoader[4], item);
                     break;
                 case 'scss':
-                    mergeLoader(defaultLoader[5], item, true, server);
+                    mergeLoader(defaultLoader[5], item);
                     break;
                 case 'less':
-                    mergeLoader(defaultLoader[6], item, true, server);
+                    mergeLoader(defaultLoader[6], item);
                     break;
                 case 'img':
                     mergeLoader(defaultLoader[7], item);
@@ -92,22 +87,45 @@ module.exports = function(config, server) {
                 default:
             }
         });
-    //添加externals 合并
-    config.externals = userConfig.externals
-        ? Object.assign(config.externals || {}, userConfig.externals)
-        : config.externals;
-    //添加extensions 合并去重
-    config.resolve.extensions =
-        userConfig.extensions && userConfig.extensions.length
-            ? Array.from(new Set([...config.resolve.extensions, ...userConfig.extensions]))
-            : config.resolve.extensions;
-    //添加alias 覆盖
-    config.resolve.alias = userConfig.alias || config.resolve.alias;
-    //添加devServer 覆盖
-    config.devServer = userConfig.devServer || config.devServer;
-    //添加plugin 合并
-    config.plugins = userConfig.plugins
-        ? config.plugins.concat(userConfig.plugins)
-        : config.plugins;
+}
+module.exports = function(config, server) {
+    if (!fs.existsSync(userWebpackConfigPath)) {
+        return config;
+    }
+    delete require.cache[require.resolve(userWebpackConfigPath)];
+    const userConfig = require(userWebpackConfigPath).webpack;
+    if (!userConfig) return config;
+    Object.entries(userConfig).map(([key, val]) => {
+        switch (key) {
+            case 'loader':
+                loaderConfig(val, config);
+                break;
+            case 'externals':
+                //添加externals 合并
+                config.externals = val
+                    ? Object.assign(config.externals || {}, val)
+                    : config.externals;
+                break;
+            case 'extensions':
+                //添加extensions 合并去重
+                config.resolve.extensions =
+                    val && val.length
+                        ? Array.from(new Set([...config.resolve.extensions, ...val]))
+                        : config.resolve.extensions;
+                break;
+            case 'alias':
+                //添加alias 覆盖
+                config.resolve.alias = val || config.resolve.alias;
+                break;
+            case 'plugins':
+                //添加plugin 合并
+                config.plugins = val ? config.plugins.concat(val) : config.plugins;
+                break;
+            default:
+                //直接覆盖操作
+                config[key] = val || config[key] || '';
+                break;
+        }
+    });
     return config;
 };
